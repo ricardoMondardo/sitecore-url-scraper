24bcec735e81a66e7906431e44ee19714a963aec
rmondardo <ricardo.mondardo@valtech.com>
2021-07-07 13:09:21 -0300

    First commit
    First commit

---
+++ url.scraper.api/Controllers/UrlScraperController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace url.scraper.api.Controllers
+{
+    public class UrlScraperController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Scraper(string url)
+        {
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(new JavaScriptSerializer().Serialize(
+                    new { data = url }), Encoding.UTF8, "application/json")
+            };
+            return result;
+        }
+    }
+}
---
+++ url.scraper.api/Pipelines/WebApi/RegisterWebApi.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Pipelines;
+using System.Web.Http;
+
+namespace url.scraper.api.Pipelines.WebApi
+{
+    public class RegisterWebApi
+    {
+        public void Process(PipelineArgs args)
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            config.Routes.MapHttpRoute("UrlScraperApiRoute",
+                "api/UrlScraperApi/{action}/{url}",
+                new { controller = "UrlScraper", action = "All", url = RouteParameter.Optional});
+
+        }
+    }
+}
