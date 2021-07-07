9bd95bdd49a34c618259168fae07eacf54da720f
rmondardo <ricardo.mondardo@valtech.com>
2021-07-07 14:12:19 -0300

    reading page as string
    reading page as string

--- url.scraper.api/Controllers/UrlScraperController.cs
+++ url.scraper.api/Controllers/UrlScraperController.cs
@@ -1,20 +1,27 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using url.scraper.api.Services;
 
 namespace url.scraper.api.Controllers
 {
     public class UrlScraperController : ApiController
     {
         [HttpGet]
-        public HttpResponseMessage Scraper(string url)
+        public async Task<HttpResponseMessage> Scraper(string url)
         {
+
+            ScraperService scraperService = new ScraperService();
+
+            var dataInfo = await scraperService.GetWordsAndImgs(url);
+
             var result = new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent(new JavaScriptSerializer().Serialize(
-                    new { data = url }), Encoding.UTF8, "application/json")
+                    new { data = dataInfo }), Encoding.UTF8, "application/json")
             };
             return result;
         }
---
+++ url.scraper.api/Services/scraperService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace url.scraper.api.Services
+{
+    public class ScraperService
+    {
+        public ScraperService() { }
+
+        public async Task<List<string>> GetWordsAndImgs(string pageUrl)
+        {
+
+            var BaseUrl = string.Format("https://{0}", pageUrl);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(BaseUrl);
+
+            var resp = await response.Content.ReadAsStringAsync();
+
+            var result = new List<string>()
+            {
+                "aaa",
+                "vvvv",
+                BaseUrl,
+                resp
+            };
+            return result;
+        }
+    }
+}
