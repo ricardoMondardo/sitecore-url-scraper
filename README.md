1b2c59be12a01a1c7e2a9f4f032a49d4aaaaeb24
rmondardo <ricardo.mondardo@valtech.com>
2021-07-08 18:29:25 -0300

    updates
    Updates

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
   
 Sitecore version: Sitecore.NET 9.2.0 (rev. 002893)
 .Net Framework : 4.7.1  
+
+
+****
+
+1 Graph: top 10 used words
+
+2 All of the images used 
+( images that are set using JS, after page load are not listed )
--- url.scraper.website/Assets/MainScraper.js
+++ url.scraper.website/Assets/MainScraper.js
@@ -13,53 +13,78 @@ async function CallApi() {
         redirect: "follow",
         referrer: "no-referrer"
     };
+
+    const chart = document.getElementById('chart-words');
+    const msgCall = document.getElementById('msg-call');
+    const GridImgs = document.getElementById('grid-imgs');
+
     let url = document.getElementById('input-url').value;
 
+
+    url = url.replace("https", "").replace("http", "");
+    url = url.replace("://", "").replace(":/", "");
+
+    chart.style.display = "none";
+
+    GridImgs.innerHTML = "";
+    msgCall.innerHTML = "Loading..."
+
     try {
+
         let resp = await fetch(`/api/UrlScraperApi/Scraper/${url}`, requestOptions)
-        let data = await resp.json();
+        
+        if (resp.ok) {
 
-        let xValues = data.ListWords.map((x) => { return x.word;})
-        let yValues = data.ListWords.map((x) => { return x.count })
+            let data = await resp.json();
+            if (data) {
 
-        let MaxValue = yValues[0];
-        let MinValue = yValues[yValues.length-1];
+                msgCall.innerHTML = "";
 
-        console.log(MaxValue, MinValue);
+                let xValues = data.ListWords.map((x) => { return x.word; })
+                let yValues = data.ListWords.map((x) => { return x.count })
 
-        new Chart("myChart", {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "rgba(0,0,255,1.0)",
-                    borderColor: "rgba(0,0,255,0.1)",
-                    data: yValues
-                }]
-            },
-            options: {
-                legend: { display: false },
-                scales: {
-                    yAxes: [{ ticks: { min: MinValue, max: MaxValue } }],
+                let MaxValue = yValues[0];
+                let MinValue = yValues[yValues.length - 1];
+
+                chart.style.display = "block";
+
+                new Chart("chart-words", {
+                    type: "line",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                            fill: false,
+                            lineTension: 0,
+                            backgroundColor: "rgba(0,0,255,1.0)",
+                            borderColor: "rgba(0,0,255,0.1)",
+                            data: yValues
+                        }]
+                    },
+                    options: {
+                        legend: { display: false },
+                        scales: {
+                            yAxes: [{ ticks: { min: MinValue, max: MaxValue } }],
+                        }
+                    }
+                });
+                                
+                for (let [key, value] of Object.entries(data.ListImages)) {
+                    GridImgs.innerHTML += `<img src=${value}>`;
+                    console.log(value);
                 }
+
+            } else {
+                console.log(resp);
+                msgCall.innerHTML = "Cannot find the website document."
             }
-        });
-
-        const GridImgs = document.getElementById('grid-imgs');
-        GridImgs.innerHTML = "";
-        for (let [key, value] of Object.entries(data.ListImages)) {
-            GridImgs.innerHTML +=
-                `<img src=${value}>`;
-            console.log(value);
+        } else {
+            console.log(resp);
+            msgCall.innerHTML = "Please, check the address and try again."
         }
-
-        console.log('Data', data);
                
-
     } catch (error) {
-        console.error(error);
+        console.log(error);
+        msgCall.innerHTML = "Please, check the address and try again."
     }
     
 }
